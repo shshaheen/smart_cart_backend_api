7e250d0f34cef80ddbcc2a5ee2eb658e739efb98
shshaheen <kolimishaheen9885@gmail.com>
2025-05-15 11:34:09 +0530

    sending data in json format

--- routes/order.js
+++ routes/order.js
@@ -144,7 +144,7 @@ orderRouter.patch('/api/orders/:id/processing', async (req, res) => {
 orderRouter.get('/api/orders', async (req, res) => {
     try {
         const orders = await Order.find();    
-        return res.status(200).send(orders);
+        return res.status(200).json(orders);
     } catch (e) {
         res.status(500).json({error: e.message});
     }
